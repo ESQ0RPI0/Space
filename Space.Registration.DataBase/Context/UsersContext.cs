a05b5a05c37943b7daa08958726cf3674a237c40
grivdm <grivdm94@gmail.com>
2023-04-26 15:59:04 -0400

    First commit in reg branch

---
+++ Space.Registration.DataBase/Context/UsersContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Space.Server.Datamodel.DatabaseModels.Registration;
+
+namespace Space.Registration.DataBase.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+               .HasKey(u => u.Id);
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ Space.Registration/Endpoints/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Space.Forms.Registration;
+using Space.Registration.DataBase.Context;
+using Space.Server.Datamodel.DatabaseModels.Registration;
+
+namespace Space.Registration.Endpoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly UsersContext _context;
+        private readonly IPasswordHasher<UserRegistrationForm> _passwordHasher;
+
+        public RegistrationController(UsersContext context, IPasswordHasher<UserRegistrationForm> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register([FromBody]UserRegistrationForm user)
+        {
+            var hashedPassword = _passwordHasher.HashPassword(user, user.Password);
+
+            var newUser = new User
+            {
+                Email = user.Email,
+                PasswordHash = hashedPassword,
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            return Ok(newUser);
+        }
+
+    }
+}
