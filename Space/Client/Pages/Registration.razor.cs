458642382245ddbd932f4fd1ccb13a751e422690
Алексей Агапов <agapov-alexei@mail.ru>
2023-05-05 22:40:57 +0300

    change httpclient to front-specific client

--- Space/Client/Pages/Registration.razor.cs
+++ Space/Client/Pages/Registration.razor.cs
@@ -1,20 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.JSInterop;
-using Space.Client;
-using Space.Client.Shared;
 using Space.Forms.Registration;
-using MudBlazor;
 
 namespace Space.Client.Pages
 {
@@ -23,7 +7,7 @@ namespace Space.Client.Pages
         private UserRegistrationForm userRegistrationForm = new UserRegistrationForm();
         public async Task RegisterAsync()
         {
-            var result = await httpClient.PostAsJsonAsync("url", userRegistrationForm);
+            var result = await httpClient.Post<bool, UserRegistrationForm>("url", userRegistrationForm);
         }
     }
 }
