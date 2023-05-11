9f8546a80a43c471729ef79169d212eab9b98601
grivdm <grivdm94@gmail.com>
2023-05-11 15:46:52 -0400

    MudBlazor integration

--- Space.Registration/Endpoints/RegistrationController.cs
+++ Space.Registration/Endpoints/RegistrationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Space.Forms.Registration;
 using Space.Registration.DataBase.Context;
 using Space.Server.Datamodel.DatabaseModels.Registration;
@@ -26,6 +27,11 @@ namespace Space.Registration.Endpoints
         [AllowAnonymous]
         public async Task<IActionResult> Register([FromBody]UserRegistrationForm user)
         {
+            if (!await IsEmailUnique(user.Email))
+            {
+                return BadRequest("Email is already in use.");
+            }
+
             var hashedPassword = _passwordHasher.HashPassword(user, user.Password);
 
             var newUser = new User
@@ -37,7 +43,12 @@ namespace Space.Registration.Endpoints
             _context.Users.Add(newUser);
             await _context.SaveChangesAsync();
 
-            return Ok();
+            return Ok("super");
+        }
+
+        private async Task<bool> IsEmailUnique(string email)
+        {
+            return await _context.Users.AllAsync(u => u.Email != email);
         }
 
     }
--- Space/Client/Pages/Registration.razor.cs
+++ Space/Client/Pages/Registration.razor.cs
@@ -6,12 +6,15 @@ namespace Space.Client.Pages
 {
     public partial class Registration
     {
+        MudForm form;
+        bool success;
+        string[] errors = { };
         private UserRegistrationForm userRegistrationForm = new UserRegistrationForm();
 
   
         public async Task RegisterAsync()
         {
-            var result = await httpClient.Post<bool, UserRegistrationForm>("https://localhost:7272/api/registration", userRegistrationForm);
+            var result = await httpClient.Post<string, UserRegistrationForm>("https://localhost:7272/api/registration", userRegistrationForm);
 
 
         }
