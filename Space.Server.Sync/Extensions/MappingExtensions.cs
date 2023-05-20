65299b7850e062df983514985a7021ddb36a9c5a
Алексей Агапов <agapov-alexei@mail.ru>
2023-05-21 00:43:55 +0300

    some changes in external model

--- Space.Server.Sync/Extensions/MappingExtensions.cs
+++ Space.Server.Sync/Extensions/MappingExtensions.cs
@@ -8,19 +8,25 @@ namespace Space.Server.Sync.Extensions
 {
     public class NewSpaceMappingProfile : Profile
     {
+        private static char[] MoneyMultipliers = new char[]{'M', 'B'};
+        private static char DefaultMultiplier = '$';
+
         public NewSpaceMappingProfile()
         {
             CreateMap<HtmlNodeCollection, NewSpaceExternalListItemModel>()
                 .ForMember(u => u.Organization, opts => opts.MapFrom(v => v[0].InnerText))
                 .ForMember(u => u.Launcher, opts => opts.MapFrom(v => v[1].InnerText))
                 .ForMember(u => u.Founded, opts => opts.MapFrom(v => int.Parse(v[2].InnerText)))
+                .ForMember(u => u.ItemCurrency, opts => opts.MapFrom(v => DefaultMultiplier))//temporary
                 .ForMember(u => u.Status, opts => opts.MapFrom(v => (LaunchVehicleStatus)Enum.Parse(typeof(LaunchVehicleStatus), v[3].InnerText)))
                 .ForMember(u => u.FirstLaunch, opts => opts.MapFrom(v => v[4].InnerText))
                 .ForMember(u => u.Launches, opts => opts.MapFrom(v => int.Parse(v[5].InnerText)))
                 .ForMember(u => u.Cost, opts => opts.MapFrom(v => decimal.Parse(string.Join("", v[6].InnerText.Where(z => char.IsDigit(z))))))
+                .ForMember(u => u.CostMultiplier, opts => opts.MapFrom(v => v[6].InnerText.FirstOrDefault(z => MoneyMultipliers.Contains(z), DefaultMultiplier)))
                 .ForMember(u => u.Perfomance, opts => opts.MapFrom(v => int.Parse(string.Join("", v[7].InnerText.TakeWhile(z => char.IsDigit(z))))))
                 .ForMember(u => u.PricePerKg, opts => opts.MapFrom(v => decimal.Parse(string.Join("", v[8].InnerText.TakeWhile(z => char.IsDigit(z))))))
                 .ForMember(u => u.Funding, opts => opts.MapFrom(v => v[9].InnerText.Trim()))
+                .ForMember(u => u.FundingMultiplier, opts => opts.MapFrom(v => v[9].InnerText.FirstOrDefault(z => MoneyMultipliers.Contains(z), DefaultMultiplier)))
                 .ForMember(u => u.Logo, opts => opts.MapFrom(v => v[10].FirstChild.GetAttributeValue("href", "")))
                 .ForMember(u => u.Photo, opts => opts.MapFrom(v => v[11].FirstChild.GetAttributeValue("href", "")));
 
