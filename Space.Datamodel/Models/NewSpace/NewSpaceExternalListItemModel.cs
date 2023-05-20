cdd697f0041a56558f4d9058a831c1c04f75af73
Алексей Агапов <agapov-alexei@mail.ru>
2023-05-21 00:32:42 +0300

    new migration for new NewSpace table

--- Space.Datamodel/Models/NewSpace/NewSpaceExternalListItemModel.cs
+++ Space.Datamodel/Models/NewSpace/NewSpaceExternalListItemModel.cs
@@ -8,12 +8,15 @@ namespace Space.Backend.Datamodel.Models.NewSpace
         public string? Launcher { get; set; }
         public int? Founded { get; set; }
         public LaunchVehicleStatus Status { get; set; }
-        public string FirstLaunch { get; set; }
+        public char? ItemCurrency { get; set; }
+        public string? FirstLaunch { get; set; }
         public int Launches { get; set; }
         public decimal? Cost { get; set; }
+        public char? CostMultiplier { get; set; }
         public int? Perfomance { get; set; }
         public decimal? PricePerKg { get; set; }
         public string Funding { get; set; }
+        public char? FundingMultiplier { get; set; }
         public string? Logo { get; set; }
         public string? Photo { get; set; }
     }
