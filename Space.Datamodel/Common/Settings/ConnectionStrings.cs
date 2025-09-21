7a3feca5fe41e46872aee245a487acde74abb068
a.agapov <esq0rpi0@gmail.com>
2025-09-21 19:38:53 +0300

    new summary for connection strings class

--- Space.Datamodel/Common/Settings/ConnectionStrings.cs
+++ Space.Datamodel/Common/Settings/ConnectionStrings.cs
@@ -1,5 +1,8 @@
 ﻿namespace Space.Server.Datamodel.Common.Settings
 {
+    /// <summary>
+    /// Connection strings for different data sources
+    /// </summary>
     public class ConnectionStrings
     {
         public string NewSpaceConnectionString { get; set; }
