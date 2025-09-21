e6c12ead7705b4fa0b81e93e04213e5a667fa040
a.agapov <esq0rpi0@gmail.com>
2025-09-21 19:36:02 +0300

    summary for registration form

--- Space.Forms/Registration/UserRegistrationForm.cs
+++ Space.Forms/Registration/UserRegistrationForm.cs
@@ -1,8 +1,17 @@
 ﻿namespace Space.Forms.Registration
 {
+    /// <summary>
+    /// Registration form
+    /// </summary>
     public class UserRegistrationForm
     {
+        /// <summary>
+        /// User email
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// Plain password
+        /// </summary>
         public string Password { get; set; }
     }
 }
